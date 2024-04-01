3daf1ae233ecf4747722e1a8dd5ceb9b3881b81b
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2024-04-01 16:40:18 +0530

    Adding type definition to Req objects middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+import NextAuth, { Session } from "next-auth";
 
 import authConfig from "@/auth.config";
 import {
@@ -7,12 +7,25 @@ import {
   authRoutes,
   publicRoutes,
 } from "./routes";
+import { NextURL } from "next/dist/server/web/next-url";
+import { RequestData } from "next/dist/server/web/types";
+import { RequestCookies } from "next/dist/compiled/@edge-runtime/cookies";
 
 const { auth } = NextAuth(authConfig);
+
+export interface LoggedIn {
+  session?: Session & { user?: any };
+  cookies?: RequestCookies;
+  geo?: RequestData["geo"];
+  ip?: string;
+  url: string;
+  nextUrl: NextURL;
+}
 
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
+  console.log("isLoggedIn", isLoggedIn);
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
 
